928e03d7bb8757794548c250f0fed987fc590af8
adescrib <adri.escribano.corraliza@gmail.com>
2023-09-17 11:27:04 +0200

    a

--- progC/Supercalculadora/Calculadora.h
+++
@@ -1,7 +0,0 @@
-#ifndef CALCULADORA_H
-#define CALCULADORA_H
-
-float calcular(float num1, float num2, char operador);
-
-#endif
-
--- progC/Supercalculadora/Conversiones.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONVERSIONES_H
-#define CONVERSIONES_H
-
-float celsius_a_fahrenheit(float celsius);
-float fahrenheit_a_celsius(float fahrenheit);
-float celsius_a_kelvin(float celsius);
-
-#endif
-
